2faf4e5154d27cd9c4a375f600a0d6ccd770da25
santileortiz <santileortiz@gmail.com>
2014-05-01 15:44:03 -0500

    agregando funciones para los servos

--- Source/main.c
+++ Source/main.c
@@ -38,6 +38,117 @@ uint32_t packet_receive=1;
 __IO uint16_t  ADC1ConvertedValue = 0, ADC1ConvertedValue2 = 0, axisValueX = 0, axisValueY = 0, calibration_value = 0, calibration_value2 = 0;
 __IO uint32_t TimingDelay = 0;
 
+static void Servos_Config(void)
+{
+  GPIO_InitTypeDef GPIO_InitStructure;
+  TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+  TIM_OCInitTypeDef  TIM_OCInitStructure;
+  uint16_t TimerPeriod = 0;
+  uint16_t Channel1Pulse = 0, Channel2Pulse = 0;
+
+  /* GPIOA and GPIOB Clocks enable */
+  RCC_AHBPeriphClockCmd( RCC_AHBPeriph_GPIOE | RCC_AHBPeriph_GPIOB, ENABLE);
+  
+  /* GPIOA Configuration: Channel 1 and 2 as alternate function push-pull */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_11;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP ;
+  GPIO_Init(GPIOE, &GPIO_InitStructure);
+  
+  GPIO_PinAFConfig(GPIOE, GPIO_PinSource9, GPIO_AF_2);
+  GPIO_PinAFConfig(GPIOE, GPIO_PinSource11, GPIO_AF_2);
+
+  
+    /* TIM1 Configuration ---------------------------------------------------
+   Generate 7 PWM signals with 4 different duty cycles:
+   TIM1 input clock (TIM1CLK) is set to APB2 clock (PCLK2)    
+    => TIM1CLK = PCLK2 = SystemCoreClock
+   TIM1CLK = SystemCoreClock, Prescaler = 0, TIM1 counter clock = SystemCoreClock
+   SystemCoreClock is set to 72 MHz for STM32F30x devices
+   
+   The objective is to generate 7 PWM signal at 17.57 KHz:
+     - TIM1_Period = (SystemCoreClock / 17570) - 1
+   The channel 1 and channel 1N duty cycle is set to 50%
+   The channel 2 and channel 2N duty cycle is set to 37.5%
+   The channel 3 and channel 3N duty cycle is set to 25%
+   The channel 4 duty cycle is set to 12.5%
+   The Timer pulse is calculated as follows:
+     - ChannelxPulse = DutyCycle * (TIM1_Period - 1) / 100
+   
+   Note: 
+    SystemCoreClock variable holds HCLK frequency and is defined in system_stm32f30x.c file.
+    Each time the core clock (HCLK) changes, user had to call SystemCoreClockUpdate()
+    function to update SystemCoreClock variable value. Otherwise, any configuration
+    based on this variable will be incorrect. 
+  ----------------------------------------------------------------------- */
+  /* Compute the value to be set in ARR regiter to generate signal frequency at 17.57 Khz */
+  TimerPeriod = 60000-1;
+  /* Compute CCR1 value to generate a duty cycle at 50% for channel 1 and 1N */
+  Channel1Pulse = (uint16_t) (((uint32_t) 5 * (TimerPeriod - 1)) / 10);
+  /* Compute CCR2 value to generate a duty cycle at 37.5%  for channel 2 and 2N */
+  Channel2Pulse = (uint16_t) (((uint32_t) 375 * (TimerPeriod - 1)) / 1000);
+
+  /* TIM1 clock enable */
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1 , ENABLE);
+  
+  /* Time Base configuration */
+  TIM_TimeBaseStructure.TIM_Prescaler = 24-1;
+  TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
+  TIM_TimeBaseStructure.TIM_Period = TimerPeriod;
+  TIM_TimeBaseStructure.TIM_ClockDivision = 0;
+  TIM_TimeBaseStructure.TIM_RepetitionCounter = 0;
+
+  TIM_TimeBaseInit(TIM1, &TIM_TimeBaseStructure);
+
+  /* Channel 1, 2,3 and 4 Configuration in PWM mode */
+  TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM2;
+  TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStructure.TIM_OutputNState = TIM_OutputNState_Enable;
+  TIM_OCInitStructure.TIM_Pulse = Channel1Pulse;
+  TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_Low;
+  TIM_OCInitStructure.TIM_OCNPolarity = TIM_OCNPolarity_High;
+  TIM_OCInitStructure.TIM_OCIdleState = TIM_OCIdleState_Set;
+  TIM_OCInitStructure.TIM_OCNIdleState = TIM_OCIdleState_Reset;
+
+  TIM_OC1Init(TIM1, &TIM_OCInitStructure);
+
+  TIM_OCInitStructure.TIM_Pulse = Channel1Pulse;
+  TIM_OC1Init(TIM1, &TIM_OCInitStructure);
+
+  TIM_OCInitStructure.TIM_Pulse = Channel2Pulse;
+  TIM_OC2Init(TIM1, &TIM_OCInitStructure);
+
+  /* TIM1 counter enable */
+  TIM_Cmd(TIM1, ENABLE);
+
+  /* TIM1 Main Output Enable */
+  TIM_CtrlPWMOutputs(TIM1, ENABLE);
+}
+
+void set_motor1(float theta1){ // theta1 -> grados
+	float pulse_width;
+	if(theta1<-1.5708) pulse_width = 1516.070;
+	else if (theta1>1.5708) pulse_width = 7369.5499;
+	else pulse_width =	3*(621.0734637f*(theta1+1.5708) + 505.356813f); // == 10.8398*thetagrad + 505.357 (el 3 convierte los us a ciclos del CPU)
+	TIM1->CCR1 = (int)pulse_width;
+}
+
+void set_motor2(float theta1){ // theta1 -> grados
+	float pulse_width;
+	if(theta1<-1.5708) pulse_width = 1544.032258;
+	else if (theta1>1.5708) pulse_width = 7350.48387;
+	else pulse_width =	3*(616.08365f*(theta1+1.5708) + 514.677f); // == 10.8398*thetagrad + 505.357 (el 3 convierte los us a ciclos del CPU)
+	TIM1->CCR2 = (int)pulse_width;
+}
+
+void delaybyms(unsigned int j){
+	unsigned int k;
+	while(j--)
+    for(k=10283;k!=0;k--); //10283 7198
+}
+
 void Delay(__IO uint32_t nTime)
 { 
   TimingDelay = nTime;
@@ -338,6 +449,7 @@ int main(void)
   Set_USBClock();
   USB_Interrupts_Config();
   USB_Init();
+	Servos_Config();
   /*!< At this stage the microcontroller clock setting is already configured, 
        this is done through SystemInit() function which is called from startup
        file (startup_stm32f30x.s) before to branch to application main.
