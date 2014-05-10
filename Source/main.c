79be361f6c1018c464221e4c8f12e76999fb5619
grisel <grisel.mortiz@gmail.com>
2014-05-09 19:01:59 -0500

    final para el CD

--- Source/main.c
+++ Source/main.c
@@ -1,9 +1,9 @@
 /**
   ******************************************************************************
-  * @file    ADC_Reading X and Y axis voltage value/main.c 
-  * @author  MCD Application Team
+  * @file   Proyecto de Control: Ball and Beam
+  * @author  Grisel Morales, Santiago León, Alejandro Juárez, Gabriela Ramos, Caroline Siordia
   * @version V1.1.0
-  * @date    7-April-2014
+  * @date    09 de Mayo de 2014
   * @brief   Main program body
   ******************************************************************************
 
@@ -57,10 +57,8 @@ static void Servos_Config(void)
   uint16_t TimerPeriod = 0;
   uint16_t Channel1Pulse = 0, Channel2Pulse = 0;
 
-  /* GPIOA and GPIOB Clocks enable */
   RCC_AHBPeriphClockCmd( RCC_AHBPeriph_GPIOE | RCC_AHBPeriph_GPIOB, ENABLE);
   
-  /* GPIOA Configuration: Channel 1 and 2 as alternate function push-pull */
   GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_11;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
@@ -71,30 +69,6 @@ static void Servos_Config(void)
   GPIO_PinAFConfig(GPIOE, GPIO_PinSource9, GPIO_AF_2);
   GPIO_PinAFConfig(GPIOE, GPIO_PinSource11, GPIO_AF_2);
 
-  
-    /* TIM1 Configuration ---------------------------------------------------
-   Generate 7 PWM signals with 4 different duty cycles:
-   TIM1 input clock (TIM1CLK) is set to APB2 clock (PCLK2)    
-    => TIM1CLK = PCLK2 = SystemCoreClock
-   TIM1CLK = SystemCoreClock, Prescaler = 0, TIM1 counter clock = SystemCoreClock
-   SystemCoreClock is set to 72 MHz for STM32F30x devices
-   
-   The objective is to generate 7 PWM signal at 17.57 KHz:
-     - TIM1_Period = (SystemCoreClock / 17570) - 1
-   The channel 1 and channel 1N duty cycle is set to 50%
-   The channel 2 and channel 2N duty cycle is set to 37.5%
-   The channel 3 and channel 3N duty cycle is set to 25%
-   The channel 4 duty cycle is set to 12.5%
-   The Timer pulse is calculated as follows:
-     - ChannelxPulse = DutyCycle * (TIM1_Period - 1) / 100
-   
-   Note: 
-    SystemCoreClock variable holds HCLK frequency and is defined in system_stm32f30x.c file.
-    Each time the core clock (HCLK) changes, user had to call SystemCoreClockUpdate()
-    function to update SystemCoreClock variable value. Otherwise, any configuration
-    based on this variable will be incorrect. 
-  ----------------------------------------------------------------------- */
-  /* Compute the value to be set in ARR regiter to generate signal frequency at 17.57 Khz */
   TimerPeriod = 60000-1;
   /* Compute CCR1 value to generate a duty cycle at 50% for channel 1 and 1N */
   Channel1Pulse = (uint16_t) (((uint32_t) 5 * (TimerPeriod - 1)) / 10);
@@ -185,175 +159,155 @@ void convParamSend(void){
 	Send_Buffer[5]=ServoY;
 
 }
-	
+
+/* Configuración pin analógico */
 static void setupGpioAnalogPC4(void){
 	
 	GPIO_InitTypeDef      GPIO_InitStructure;
-	/*GPIOA Periph clock enable */
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
-		
-	/* Configure ADC2 Channel1 as analog input-->PA4 */
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4 ;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
+
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4 ; // Pin 4 del puerto C
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN; // Analógico
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL ;
   GPIO_Init(GPIOC, &GPIO_InitStructure);
 }
 
+/* Configuración pin analógico */
 static void setupGpioAnalogPF4(void){
 	
 	GPIO_InitTypeDef      GPIO_InitStructure;
-	/*GPIOA Periph clock enable */
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOF, ENABLE);
-	
-  /* Configure ADC1 Channel7 as analog input-->PC1 */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4 ;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
+
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4 ; // Pin 4 del puerto F
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN; // Analógico
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL ;
   GPIO_Init(GPIOF, &GPIO_InitStructure);
 
 }
 
+/* Configuración pin alta impedancia */
 static void setupGpioHZPD8(void){
 	
 	GPIO_InitTypeDef      GPIO_InitStructure;
-	/*GPIOA Periph clock enable */
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOD, ENABLE);
-		
-	/* Configure ADC2 Channel1 as analog input-->PA4 */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8; // Pin 8 del puerto D
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN; // Entrada digital
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIOD->PUPDR &= 0xFFFFFFF3;
 	GPIOD->MODER &= 0xFFFFFFF3;
 	GPIO_Init(GPIOD, &GPIO_InitStructure);
 }
 
+/* Configuración pin alta impedancia */
 static void setupGpioHZPB0(void){
 	
 	GPIO_InitTypeDef      GPIO_InitStructure;
-	/*GPIOA Periph clock enable */
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
 		
-	/* Configure ADC2 Channel1 as analog input-->PA4 */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0; // Pin 0 del puerto B
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN; //Entrada digital
   GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIOB->PUPDR &= 0xFFFFFFF3;
 	GPIOB->MODER &= 0xFFFFFFF3;
 	GPIO_Init(GPIOB, &GPIO_InitStructure);
 }
 
+/* Configuración pin salida digital */
 static void setupGpioPC4Output(void){
 	
 	GPIO_InitTypeDef      GPIO_InitStructure;
-	/*GPIOA Periph clock enable */
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOC, ENABLE);
 	
-  /* Configure PA4 in output pushpull mode */
-
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4; // Pin 4 del puerto C
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT; //Salida digital
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIO_Init(GPIOC, &GPIO_InitStructure);
 }
 
+/* Configuración pin salida digital */
 static void setupGpioPF4Output(void){
 	
 	GPIO_InitTypeDef      GPIO_InitStructure;
-	/*GPIOA Periph clock enable */
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOF, ENABLE);
 	
-	/* Configure PA4 in output pushpull mode */
-
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4; // Pin 4 del puerto F
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT; //Salida digital
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIO_Init(GPIOF, &GPIO_InitStructure);
 }
 
+/* Configuración pin salida digital */
 static void setupGpioPD8Output(void){
 	
 	GPIO_InitTypeDef      GPIO_InitStructure;
-	/*GPIOA Periph clock enable */
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOD, ENABLE);
-	
-  /* Configure PA4 in output pushpull mode */
 
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8; //Pin 8 del puerto D
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;  //Salida digital
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIO_Init(GPIOD, &GPIO_InitStructure);
 }
 
+/* Configuración pin salida digital */
 static void setupGpioPB0Output(void){
 	
 	GPIO_InitTypeDef      GPIO_InitStructure;
-	/*GPIOA Periph clock enable */
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOB, ENABLE);
-	
-  /* Configure PA4 in output pushpull mode */
 
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0; //Pin 0 del puerto B
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT; //Salida digital
 	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
 	GPIO_Init(GPIOB, &GPIO_InitStructure);
 }
 
+/* Procedimiento de calibración del ADC1 */
 static void calibrateADC1(void){
 
-  /* Calibration procedure */  
   ADC_VoltageRegulatorCmd(ADC1, ENABLE);
-  
-  /* Insert delay equal to 10 µs */
   delaybyus(10);
   
   ADC_SelectCalibrationMode(ADC1, ADC_CalibrationMode_Single);
   ADC_StartCalibration(ADC1);
 	
-	while(ADC_GetCalibrationStatus(ADC1) != RESET ){}
+	while(ADC_GetCalibrationStatus(ADC1) != RESET ){} //Espera a que se calibre
   calibration_value = ADC_GetCalibrationValue(ADC1); 
 }
-
+/* Procedimiento de calibración del ADC2 */
 static void calibrateADC2(void){
 	
-  /* Calibration procedure */  
   ADC_VoltageRegulatorCmd(ADC2, ENABLE);
-  
-  /* Insert delay equal to 10 µs */
   delaybyus(10);
  	
-	ADC_SelectCalibrationMode(ADC2, ADC_CalibrationMode_Single);
+	ADC_SelectCalibrationMode(ADC2, ADC_CalibrationMode_Single); 
   ADC_StartCalibration(ADC2);
 	
-	while(ADC_GetCalibrationStatus(ADC2) != RESET ){}
+	while(ADC_GetCalibrationStatus(ADC2) != RESET ){} //Espera a que se calibre
   calibration_value2 = ADC_GetCalibrationValue(ADC2);
 }
 
+/*Configuración de ADC para el PF4 donde se leerá un electródod del panel*/
 static void setupAdcPF4(void){
 	ADC_InitTypeDef       ADC_InitStructure;
 	ADC_CommonInitTypeDef ADC_CommonInitStructure;
-	
-	/* Configure the ADC clock */
   RCC_ADCCLKConfig(RCC_ADC12PLLCLK_Div2);
-	  
-  /* Enable ADC1 and ADC2 clock */
   RCC_AHBPeriphClockCmd(RCC_AHBPeriph_ADC12, ENABLE);
-	
-	/* Calling functions */
+
+/* Llamando funciones que configuran el pin como analógico */
 	setupGpioAnalogPF4();
 	calibrateADC1();  
 
   ADC_StructInit(&ADC_InitStructure);
 
-	/* ADC1 configuration */
+	/*Configuración del ADC*/
 	ADC_CommonInitStructure.ADC_Mode = ADC_Mode_Independent;                                                                    
   ADC_CommonInitStructure.ADC_Clock = ADC_Clock_AsynClkMode;                    
   ADC_CommonInitStructure.ADC_DMAAccessMode = ADC_DMAAccessMode_Disabled;             
@@ -361,8 +315,6 @@ static void setupAdcPF4(void){
   ADC_CommonInitStructure.ADC_TwoSamplingDelay = 0;          
   ADC_CommonInit(ADC1, &ADC_CommonInitStructure);
 	
-	/* ADC1 configuration */
-//  ADC_InitStructure.ADC_ContinuousConvMode = ADC_ContinuousConvMode_Enable; //CULPABLE
   ADC_InitStructure.ADC_Resolution = ADC_Resolution_12b; 
   ADC_InitStructure.ADC_ExternalTrigConvEvent = ADC_ExternalTrigConvEvent_0;         
   ADC_InitStructure.ADC_ExternalTrigEventEdge = ADC_ExternalTrigEventEdge_None;
@@ -372,38 +324,36 @@ static void setupAdcPF4(void){
   ADC_InitStructure.ADC_NbrOfRegChannel = 1; 
   ADC_Init(ADC1, &ADC_InitStructure);
   
-  /* ADC1 regular channel7 configuration */ 
+/*Se lee el canal 5*/
   ADC_RegularChannelConfig(ADC1, ADC_Channel_5, 1, ADC_SampleTime_7Cycles5);
    
-  /* Enable ADC1 and ADC2 */
+/* Se habilita el ADC1 */
   ADC_Cmd(ADC1, ENABLE);
   
-  /* wait for ADRDY */
+/* espera por ADRDY */
   while(!ADC_GetFlagStatus(ADC1, ADC_FLAG_RDY));
   
-  /* Start ADC1 and ADC2 Conversion */ 
+/* Comienza la conversión del ADC*/
   ADC_StartConversion(ADC1);   
 	
 }
+
+/*Configuración de ADC para el PC4 donde se leerá un electródod del panel*/
 
 static void setupAdcPC4(void){
 	ADC_InitTypeDef       ADC_InitStructure;
 	ADC_CommonInitTypeDef ADC_CommonInitStructure;
 	
-	/* Configure the ADC clock */
   RCC_ADCCLKConfig(RCC_ADC12PLLCLK_Div2);
-	  
-  /* Enable ADC1 and ADC2 clock */
   RCC_AHBPeriphClockCmd(RCC_AHBPeriph_ADC12, ENABLE);
 	
-	/* Calling functions */
+	/* Llamando funciones que configuran el pin como analógico */
 	setupGpioAnalogPC4();
 	calibrateADC2();
-  
 
   ADC_StructInit(&ADC_InitStructure);
 
-	/* ADC2 configuration */
+	/*Configuración del ADC*/
 	ADC_CommonInitStructure.ADC_Mode = ADC_Mode_Independent;                                                                    
   ADC_CommonInitStructure.ADC_Clock = ADC_Clock_AsynClkMode;                    
   ADC_CommonInitStructure.ADC_DMAAccessMode = ADC_DMAAccessMode_Disabled;             
@@ -411,8 +361,6 @@ static void setupAdcPC4(void){
   ADC_CommonInitStructure.ADC_TwoSamplingDelay = 0;          
   ADC_CommonInit(ADC2, &ADC_CommonInitStructure);
 	
-	/* ADC2 configuration */
-//  ADC_InitStructure.ADC_ContinuousConvMode = ADC_ContinuousConvMode_Enable; //CULPABLE
   ADC_InitStructure.ADC_Resolution = ADC_Resolution_12b; 
   ADC_InitStructure.ADC_ExternalTrigConvEvent = ADC_ExternalTrigConvEvent_0;         
   ADC_InitStructure.ADC_ExternalTrigEventEdge = ADC_ExternalTrigEventEdge_None;
@@ -422,46 +370,47 @@ static void setupAdcPC4(void){
 	ADC_InitStructure.ADC_NbrOfRegChannel = 1;
 	ADC_Init(ADC2, &ADC_InitStructure);
   
-  /* ADC2 regular channel1 configuration */ 
+/*Se lee el canal 5*/
 	ADC_RegularChannelConfig(ADC2, ADC_Channel_5, 1, ADC_SampleTime_7Cycles5);
    
-  /* Enable ADC2 */
+/* Se habilita el ADC2 */
 	ADC_Cmd(ADC2, ENABLE);
   
-  /* wait for ADRDY */
+/* espera por ADRDY */
 	while(!ADC_GetFlagStatus(ADC2, ADC_FLAG_RDY));
   
-  /* Start ADC2 Conversion */   
+/* Comienza la conversión del ADC*/
 	ADC_StartConversion(ADC2);   
 
 }
 
+/* Esta función se encarga de configurar los pines de la STM como VCC, GND, Alta impedancia y un analógico en que 
+se leerá la posición en el eje X */
 static void readXValue(void){
 
-	setupGpioHZPB0();
-	setupGpioPC4Output();
-	setupGpioPD8Output();
-	/* Set PE14 and PE15 */
+	setupGpioHZPB0(); //Llama Función que configura PB0 como alta impedancia
+	setupGpioPC4Output(); //Llama Función que configura PC4 como VCC
+	setupGpioPD8Output(); //Llama Función que configura PD8 como GND
   GPIOD->BSRR = 0x0100;
-  /* Reset PE14 and PE15 */
   GPIOC->BRR = 0x0010;
-	setupAdcPF4();
+	setupAdcPF4(); //Llama Función que configura PF4 como pin analógico
 
 }
 
+/* Esta función se encarga de configurar los pines de la STM como VCC, GND, Alta impedancia y un analógico en que 
+se leerá la posición en el eje Y */
 static void readYValue(void){
 
-	setupGpioHZPD8();
-	setupGpioPF4Output();
-	setupGpioPB0Output();
-	/* Set PE14 and PE15 */
+	setupGpioHZPD8();  //Llama Función que configura PB0 como alta impedancia
+	setupGpioPF4Output(); //Llama Función que configura PC4 como VCC
+	setupGpioPB0Output(); //Llama Función que configura PD8 como GND
   GPIOB->BSRR = 0x0001 ;
-  /* Reset PE14 and PE15 */
   GPIOF->BRR = 0x0010 ;
-	setupAdcPC4();
+	setupAdcPC4(); //Llama Función que configura PF4 como pin analógico
 
 }
 
+/*Función que rota un arreglo*/
 void rotArray(float *p_arr, int arr_len){
 	int i;
 	for(i=arr_len-1; i>0; i--){
@@ -469,42 +418,55 @@ void rotArray(float *p_arr, int arr_len){
 	}
 }
 
+/*Para implementar el control diseñado en el microcontrolador se utilizaron arreglos 
+que almacenan la versión actual de la posición de la bola y las mediciones anteriores, 
+cada iteración del bucle principal el arreglo se recorre en una posición descartando la 
+última y poniendo la nueva a la cabeza por medio de la función rotArray() que recibe un 
+pointer al inicio del arreglo que se quiere rotar así como el tamaño del mismo. 
+Las mediciones del panel se limitan a los valores que son físicamente posibles pues 
+cuando no hay ninguna bola sobre él dan mediciones fuera de las distancias caracterizadas, 
+posteriormente se rota el arreglo y se almacena la última medición a la cabeza para proceder 
+a calcular el valor de la ley de control con los valores del arreglo y las ganancias calculadas 
+del diseño de control, esta es directamente enviada a los servos por medio de la función set_motor1() 
+el factor de resta que se ve dentro de la llamada a la función es una compensación a la inclinación 
+original del sistema. Finalmente es necesario que haya unos casos que generen saturación cuando el 
+ángulo enviado a los motores sea más grande que un valor máximo. El mismo procedimiento aplica para 
+las leyes de control con las 2 coordenadas del panel como entradas.*/
+
 void leyDeControlX(float xCoord){
 	float ley, prom;
-	if(xCoord >= 0.087)
+	if(xCoord >= 0.087) // Se limita lo que se lee el panel para no tomar en cuenta el ruido
 		xCoord = xCoords[0];
-	rotArray(xCoords, 2);
-	xCoords[0] = xCoord;
-	prom = (xCoord + xCoords[1])/2;
-	
-	rotArray(Xerr, 2);
-	Xerr[0] = (SetpointX/100.0f)-prom;
-	ley = 7.7652*Xerr[0]-6.5125*Xerr[1];
-	if (ley > 0.4)
+	rotArray(xCoords, 2); //se rota el arreglo de las coordenadas, actual y anterior
+	xCoords[0] = xCoord; //se almacena la coordenada actual
+	prom = (xCoord + xCoords[1])/2; // se hace un promedio de las coordenadas para evitar picos en la lectura del panel
+	rotArray(Xerr, 2); //se rota el arreglo del error, actual y anterior
+	Xerr[0] = (SetpointX/100.0f)-prom; //Se calcula el error actual de X
+	ley = 7.7652*Xerr[0]-6.5125*Xerr[1]; //se calcula la ley de control
+	if (ley > 0.4) //se limita el ángulo enviado a los motores
 		ley = 0.4;
 	else if (ley <-0.4)
 		ley = -0.4;
 	ServomotorX=ley;
-	set_motor2(-ley);
+	set_motor2(-ley); //se envía el ángulo a los motores
 }
 
 void leyDeControlY(float yCoord){
 	float ley, prom;
-	if(yCoord >= 0.087)
+	if(yCoord >= 0.087) // Se limita lo que se lee el panel para no tomar en cuenta el ruido
 		yCoord = yCoords[0];
-	rotArray(yCoords, 2);
-	yCoords[0] = yCoord;
-	prom = (yCoord + yCoords[1])/2;
-	
-	rotArray(Yerr, 2);
-	Yerr[0] = (SetpointY/100.0f)-prom;
-	ley = 7.7652*Yerr[0]-6.5125*Yerr[1];
-	if (ley > 0.4)
+	rotArray(yCoords, 2); //se rota el arreglo de las coordenadas, actual y anterior
+	yCoords[0] = yCoord; //se almacena la coordenada actual
+	prom = (yCoord + yCoords[1])/2; // se hace un promedio de las coordenadas para evitar picos en la lectura del panel
+	rotArray(Yerr, 2); //se rota el arreglo del error, actual y anterior
+	Yerr[0] = (SetpointY/100.0f)-prom; //Se calcula el error actual de Y
+	ley = 7.7652*Yerr[0]-6.5125*Yerr[1]; //se calcula la ley de control
+	if (ley > 0.4) //se limita el ángulo enviado a los motores
 		ley = 0.4;
 	else if (ley <-0.4)
 		ley = -0.4;
 	ServomotorY=ley;
-	set_motor1(-ley+0.013);
+	set_motor1(-ley+0.013); //se envía el ángulo a los motores
 }
 
 int main(void)
@@ -538,7 +500,7 @@ int main(void)
     /* Guardando valor leido de Eje X */
     ADC1ConvertedValue =ADC_GetConversionValue(ADC1);
     
-    /* Calculando la posicion correspondiente según caracterización */
+    /* Calculando la posición correspondiente según caracterización */
 		axisValueXm = ((ADC1ConvertedValue-191.87f)/22248.0f)-0.083f;
 		
 		//Se configuran ADC para siguiente lectura
